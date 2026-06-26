2562bb27b1b373da15418728f4e82bec99bab3b4
Jesus Sugar <jesussugar27@gmail.com>
2026-06-25 21:51:25 -0500

    adding protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard/>}/>
+      
+      <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+      
     </Routes>
+    
   )
 }
 
